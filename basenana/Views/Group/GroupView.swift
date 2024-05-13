b82c65c3b40b1f4a60924d95513fc4278fcd5038
zwwhdls <zww@hdls.me>
2024-05-13 16:26:53 +0800

    fix: document height
    Signed-off-by: zwwhdls <zww@hdls.me>

--- basenana/Views/Group/GroupView.swift
+++ basenana/Views/Group/GroupView.swift
@@ -15,66 +15,67 @@ struct GroupView: View{
     @State private var selectedDoc: DocumentModel? = nil
     
     var body: some View {
-        VStack{
-            
-            VSplitView(){
-                Table(of: EntryModel.self, selection: $selection) {
-                    TableColumn("Name", value: \.name)
-                    TableColumn("Kind", value: \.kind)
-                    TableColumn("Size"){
-                        Text("\($0.size)")
-                    }
-                    TableColumn("Date Modified") {
-                        Text("\($0.modifiedAt, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    }
-                } rows: {
-                    ForEach(groupChileren) { child in
-                        TableRow(child)
-                            .contextMenu {
-                                Button("Edit") {
-                                    // TODO open editor in inspector
-                                }
-                                Button("See Details") {
-                                    // TODO open detai view
-                                }
-                                Divider()
-                                Button("Delete", role: .destructive) {
-                                }
-                            }
-                            .draggable(IDHelper(kind: child.isGroup ? "group" : "entry", id: child.id!).Encode())
-                            .dropDestination(for: String.self){ entryInfos in
-                                if !child.isGroup {
-                                    return
-                                }
-                                groupService.moveEntriesToGroup(entries: parseIDInfo(entryInfos: entryInfos), groupID: child.id!)
-                            }
-                    }
-                }
-                .dropDestination(for: String.self){ entryInfos, _ in
-                    groupService.moveEntriesToGroup(entries: parseIDInfo(entryInfos: entryInfos), groupID: groupID)
-                    return false
-                }
-                .onAppear{
-                    groupChileren = entryService.listChildren(parentEntryID: groupID)
-                }
-                .onChange(of: GroupRoot.updateAt){
-                    groupChileren = entryService.listChildren(parentEntryID: groupID)
-                    log.info("relist group \(groupID) children")
-                }
-                .frame(minHeight: 200, maxHeight: .infinity)
+        GeometryReader { geometry in
+            VStack{
                 
-                if selection.count == 1 {
-                    if let unwrappedID = selection.first {
-                        let entryId: Int64 = unwrappedID ?? 0
-                        DocumentDetailView(doc: documentService.getDocument(entryId: entryId))
-                            .frame(minHeight: 500, idealHeight: .infinity/2, maxHeight: .infinity)
-                            .layoutPriority(1)
+                VSplitView(){
+                    Table(of: EntryModel.self, selection: $selection) {
+                        TableColumn("Name", value: \.name)
+                        TableColumn("Kind", value: \.kind)
+                        TableColumn("Size"){
+                            Text("\($0.size)")
+                        }
+                        TableColumn("Date Modified") {
+                            Text("\($0.modifiedAt, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                        }
+                    } rows: {
+                        ForEach(groupChileren) { child in
+                            TableRow(child)
+                                .contextMenu {
+                                    Button("Edit") {
+                                        // TODO open editor in inspector
+                                    }
+                                    Button("See Details") {
+                                        // TODO open detai view
+                                    }
+                                    Divider()
+                                    Button("Delete", role: .destructive) {
+                                    }
+                                }
+                                .draggable(IDHelper(kind: child.isGroup ? "group" : "entry", id: child.id!).Encode())
+                                .dropDestination(for: String.self){ entryInfos in
+                                    if !child.isGroup {
+                                        return
+                                    }
+                                    groupService.moveEntriesToGroup(entries: parseIDInfo(entryInfos: entryInfos), groupID: child.id!)
+                                }
+                        }
+                    }
+                    .dropDestination(for: String.self){ entryInfos, _ in
+                        groupService.moveEntriesToGroup(entries: parseIDInfo(entryInfos: entryInfos), groupID: groupID)
+                        return false
+                    }
+                    .onAppear{
+                        groupChileren = entryService.listChildren(parentEntryID: groupID)
+                    }
+                    .onChange(of: GroupRoot.updateAt){
+                        groupChileren = entryService.listChildren(parentEntryID: groupID)
+                        log.info("relist group \(groupID) children")
+                    }
+                    .frame(minHeight: 200, maxHeight: .infinity)
+                    
+                    if selection.count == 1 {
+                        if let unwrappedID = selection.first {
+                            let entryId: Int64 = unwrappedID ?? 0
+                            DocumentDetailView(doc: documentService.getDocument(entryId: entryId))
+                                .frame(minHeight: 300, idealHeight: geometry.size.height/2)
+                                .layoutPriority(1)
+                        }
                     }
                 }
+                .layoutPriority(1)
             }
-            .layoutPriority(1)
         }
-        
     }
 }
 
